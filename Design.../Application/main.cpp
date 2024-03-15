82c516131e9e89695fd66474956967e63068c595
Asype <97271507+Asype@users.noreply.github.com>
2024-03-14 23:57:37 -0700

    Update main.cpp

--- Design.../Application/main.cpp
+++ Design.../Application/main.cpp
@@ -1,1 +1,20 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
+// Main.cpp already implemented for us
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/DBDemoBookLink/Main.qml"_qs);
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
